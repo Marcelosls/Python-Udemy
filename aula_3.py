5860e2cac6270b39b786a3a89fc9e4e27ef76f3c
marcelosls <marcelo090701@gmailc.om>
2025-05-16 15:19:28 -0300

    aula 3

---
+++ aula_3.py
@@ -0,0 +1,26 @@
+'''
+Python = Linguagem de programação
+Tipo de tipagem = Dinamica/forte(identifica automaticamente o tipo do atibuto)
+Str->String->texto
+Strings são textos que estão dentro de aspas
+'''
+print(1234)
+
+# Aspas Simples
+print('Aspas Simples')
+
+#aspas simples dentro de duplas e vice-versa para mostrar as aspas
+print('"Aspas Simples"')
+print('pessoa "inteligente"')
+
+
+# Aspas Duplas
+print("Aspas Duplas")
+
+#formas de ignorar simbolos
+
+# Escape \
+print('pessoa \"inteligente\"')
+
+# r o criador diz ser usado para expressões regulares
+print(r'Aspas \ Simples')

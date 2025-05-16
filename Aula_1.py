8636199a5e4950fb825ecc456b29d270428202e9
marcelosls <marcelo090701@gmailc.om>
2025-05-16 14:55:36 -0300

    aula 1 e 2

---
+++ Aula_1.py
@@ -0,0 +1,14 @@
+#comentario
+
+print("hello world")
+print(1+1)
+
+"""
+DocString
+escrever 
+varias
+linha
+teste
+teste
+"""
+''' Usar para escrever notas'''
---
+++ Aula_2.py
@@ -0,0 +1,9 @@
+
+'''
+Multiplos argumentos divididos por virgula
+'''
+print(12, 34)
+
+#exemplo de alteração do separador(padrão espaço vazio) ou final(padrão /r/n(pular linha))
+print(56, "teste", sep="-", end=" e, \n")
+print(78, 9, end="...")
